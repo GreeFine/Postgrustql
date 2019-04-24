f36eb7f5a27db3863487008b559b389351520671
Raphael <greefine@hotmail.fr>
2019-04-24 11:58:57 -0700

    Fast implem for pic table with new abstract

--- src/graphql/queries.rs
+++ src/graphql/queries.rs
@@ -1,3 +1,4 @@
+use super::super::database::{e_tables, requestable_data};
 use super::context::Context;
 use juniper::FieldResult;
 
@@ -15,6 +16,54 @@ pub struct FlowerDesc {
   pub name: String,
 }
 
+
+#[derive(juniper::GraphQLObject, Debug)]
+struct Picture {
+  binaire_href: String,
+  determination_ns: String,
+}
+
+#[derive(juniper::GraphQLObject, Debug)]
+struct Pictures {
+  pub nodes: Vec<Picture>,
+}
+
+impl requestable_data for Picture {
+  fn new() -> Self {
+    Picture {
+      binaire_href: String::new(),
+      determination_ns: String::new(),
+    }
+  }
+  fn create(row: &mut mysql::Row) -> Self {
+    let mut _self = Self::new();
+    _self.feed(row);
+    _self
+  }
+
+  fn feed(&mut self, row: &mut mysql::Row) -> &mut Self {
+    self.binaire_href = row.take(0).unwrap();
+    self.determination_ns = row.take(1).unwrap();
+    self
+  }
+}
+
+
+impl requestable_data for Pictures {
+  fn new() -> Self {
+    Pictures { nodes: Vec::new() }
+  }
+
+  fn create(_: &mut mysql::Row) -> Self {
+    panic!("Not implem")
+  }
+
+  fn feed(&mut self, row: &mut mysql::Row) -> &mut Self {
+    self.nodes.push(Picture::create(row));
+    self
+  }
+}
+
 impl Query {
   pub fn init() {
     juniper::graphql_object!(Query: Context |&self| {
@@ -25,6 +74,15 @@ impl Query {
         field flowers(&executor, limit: i32) -> FieldResult<Flowers> {
           let db = &executor.context().database;
           Ok(db.get_flowers(limit))
+        }
+
+        field pictures(&executor, limit: i32) -> FieldResult<Pictures> {
+          let db = &executor.context().database;
+          Ok(db.request::<Pictures>(
+            vec!["binaire_href", "determination_ns"],
+            e_tables::pictures,
+            Some(limit as u32),
+          ))
         }
     });
   }
